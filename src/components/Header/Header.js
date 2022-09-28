7cf8a7ba8a57036929c34aa0a7708dc424722538
Anamul10 <anamul.hasan@mnsu.edu>
2022-09-28 18:59:38 -0400

    title name changed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,9 +5,9 @@ import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <h1 className='text-primary m-5 mb-5'><span><FontAwesomeIcon icon={faWeightHanging}></FontAwesomeIcon></span> Active Gym Club</h1>
-            <h2>Select your Exercise:</h2>
+        <div className='ms-5 ps-4 w-75'>
+            <h1 className='text-primary m-5 mb-5 text-center'><span><FontAwesomeIcon icon={faWeightHanging}></FontAwesomeIcon></span> Active Gym Club</h1>
+            <h2 className='ms-5'>Select your Exercise:</h2>
         </div>
     );
 };
