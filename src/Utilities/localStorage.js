43c7ea6403b09b942219b5083bd62abad5bbbefd
Anamul10 <anamul.hasan@mnsu.edu>
2022-09-29 03:58:08 -0400

    struggling with local storage. Added some rough codes

---
+++ src/Utilities/localStorage.js
@@ -0,0 +1,21 @@
+// const addToDb = id => {
+
+
+//     const getStoredTime = () => {
+//         const storedTime = localStorage.getItem('break-time');
+//         let breakTime = {};
+//         if (storedTime) {
+//             breakTime = JSON.parse(storedTime);
+//         }
+//         return breakTime;
+//     }
+
+//     const addTimeToDb = (time, quantity) => {
+//         const breakTime = getStoredTime();
+//         localStorage.setItem('break-time', JSON.stringify(breakTime));
+//     }
+
+// }
+
+// export { addToDb }
+
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+// import { addToDb } from '../../Utilities/localStorage';
 
 const Profile = (props) => {
     const { time } = props;
@@ -9,6 +10,8 @@ const Profile = (props) => {
     }
 
     const [rest, setBreak] = useState(0);
+    // addToDb()
+
 
     return (
         <div>
