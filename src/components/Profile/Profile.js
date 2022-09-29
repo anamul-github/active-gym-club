00f3d686adc5b64141e26e1a4ff76a4a4027a473
Anamul10 <anamul.hasan@mnsu.edu>
2022-09-28 21:52:15 -0400

    added info to the right side in Profile component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,12 +3,12 @@ import React from 'react';
 const Profile = () => {
     return (
         <div>
-            <div className='text-center mt-3'>
+            <div className='text-center mt-5'>
                 <h4>Anamul</h4>
                 <p>Minnesota, USA</p>
             </div>
 
-            <div className='d-flex justify-content-between pt-3 shadow p-3'>
+            <div className='d-flex justify-content-between mt-5 shadow-lg p-3 rounded'>
                 <div>
                     <h4>75kg</h4>
                     <p>Weight</p>
@@ -23,25 +23,25 @@ const Profile = () => {
                 </div>
             </div>
 
-            <div className='mt-3'>
+            <div className='mt-5'>
                 <h4>Add A Break</h4>
-                <div className='d-flex justify-content-between'>
-                    <p>10s</p>
-                    <p>20s</p>
-                    <p>30s</p>
-                    <p>40s</p>
+                <div className='d-flex justify-content-between shadow-lg p-3 rounded mt-3'>
+                    <button className='border-0 rounded-pill gray-400 p-2 fw-bold'><p>10s</p></button>
+                    <button className='border-0 rounded-pill bg-primary p-2 fw-bold text-white'><p>20s</p></button>
+                    <button className='border-0 rounded-pill gray-400 p-2 fw-bold'><p>30s</p></button>
+                    <button className='border-0 rounded-pill gray-400 p-2 fw-bold'><p>40s</p></button>
                 </div>
             </div>
 
-            <h4 className='mt-3'>Exercise Details</h4>
-            <div>
+            <h4 className='mt-5'>Exercise Details</h4>
+            <div className='d-flex justify-content-between shadow-lg p-3 rounded mt-3'>
                 <h5>Exercise Time</h5>
-                <p></p>
+                <p>:</p>
             </div>
 
-            <div className='pt-3'>
+            <div className='pt-3 d-flex justify-content-between shadow-lg p-3 rounded mt-4'>
                 <h5>Break Time</h5>
-                <p></p>
+                <p>:</p>
             </div>
 
             <button className='btn btn-primary w-100 mt-5'>Activity Completed</button>
